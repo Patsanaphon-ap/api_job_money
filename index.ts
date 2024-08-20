6522184eb4f38c835a5e07457c283e855b95919c
Patsanaphon Apiwattanawong <patsanaphon_ap@gmail.com>
2024-08-20 14:51:14 +0700

    async await

--- index.ts
+++ index.ts
@@ -2,11 +2,12 @@ import * as cron from 'node-cron';
 import { manager } from './src/index';
 
   try {
-      cron.schedule('* * * 1 * *', function () {
+      cron.schedule('* * * 1 * *', async function () {
         
           // This job will run every second
           console.log(new Date().toLocaleTimeString());
-          manager.jobdaily.dailyExchangeRate();
+          await manager.jobdaily.dailyExchangeRate();
+          console.log("finish")
         });
   } catch (error) {
     console.log("Error scheduling cron job interfaceStore :", error);
