2a2ec8c87c2c2926f983d79fbb91534dd95d074c
Patsanaphon Apiwattanawong <patsanaphon_ap@gmail.com>
2024-08-29 15:16:51 +0700

    daily every month

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import * as cron from 'node-cron';
 import { manager } from './src/index';
 
   try {
-      cron.schedule('* * * 1 * *', async function () {
+      cron.schedule('0 0 1 * *', async function () {
         
           // This job will run every second
           console.log(new Date().toLocaleTimeString());
